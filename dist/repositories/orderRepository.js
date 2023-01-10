67cb725237d0dcdc5df2c96fe42a25de9df675b9
Abner Ananias <abner.dev@Air-de-Abner.Dlink>
2023-01-10 12:20:33 -0300

    ajustes

--- dist/repositories/orderRepository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.orderRespository = void 0;
-const data_source_1 = require("../data-source");
-const Order_1 = require("../entities/Order");
-exports.orderRespository = data_source_1.AppDataSource.getRepository(Order_1.Order);
